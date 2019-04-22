22587e11fb646b987c05fd072e59ee5d0cf98ddd
LLH <1037378313@qq.com>
2019-04-22 16:46:16 +0800

    swipper-finished

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import FastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import 'mystyles/reset.css'
+import 'mystyles/border.css'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
+FastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
